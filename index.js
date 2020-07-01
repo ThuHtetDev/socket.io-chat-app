737360fc0fef4dd4f67c2ef538d1db3a664922c8
ThuhtetCoder <59603198+ThuhtetCoder@users.noreply.github.com>
2020-07-01 13:24:41 +0630

    Https Socket-Server

--- index.js
+++ index.js
@@ -1,16 +1,36 @@
 const { dirname } = require('path');
 
-let app = require('express')();
-let http = require('http').Server(app);
-let io = require('socket.io')(http);
+// let app = require('express')();
+// let http = require('http').Server(app);
+// let https = require('https');
+// let http = require('http');
+// let io = require('socket.io')(http);
+let sio = require('socket.io');
+
+let fs = require('fs');
+var sys = require('sys');
+var app = require('express')();
+var https = require('https');
+var server = https.createServer({
+    key: fs.readFileSync('mykey.key'),
+    cert: fs.readFileSync('mycrt.crt'),
+    requestCert: false,
+    rejectUnauthorized: false
+},app);
+server.listen(3334,function(){
+    console.log("Connected HTTP");
+});
+
+var io = require('socket.io').listen(server);
 
 app.get('/',(req,res) => {
     res.sendFile( __dirname +'/index.html');
 });
 
-http.listen(3000, () => {
-    console.log('connected done');
-});
+// http.listen(3000, () => {
+//     console.log('connected done');
+// });
+
 
 io.on('connection',(socket) =>{
     console.log("serverconnection done");
